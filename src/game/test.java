9930922eea79e283526c6dedbde0e8558cf99ba0
Mohamed <mohamedfadl57357@gmail.com>
2023-11-02 21:11:31 +0200

    first commit

---
+++ src/game/test.java
@@ -0,0 +1,15 @@
+package game;
+
+public class test {
+	
+
+public static void main (String [] args) {
+		
+		char[][] board ={{' ', ' ', ' '},
+		                 {' ', ' ', ' '},
+		                 {' ', ' ', ' '}};
+		
+		printBoard(board);
+}
+
+}
